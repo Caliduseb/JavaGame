6ca14fe7932a0705caf911d3123dfd069e88aeb3
Caliduseb <46005624+Caliduseb@users.noreply.github.com>
2019-09-15 00:31:33 +0200

    Adding Comments to Main.java

--- com/sebi/Main.java
+++ com/sebi/Main.java
@@ -4,17 +4,15 @@ import javax.swing.*;
 import java.net.URL;
 
 public class Main {
-    private static JFrame Frame = new JFrame("Game");
+    private static JFrame Frame = new JFrame("Game");   //delcaring JFrame out of main void for easier access later
 
     public static void main(String[] args) {
+        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //initialising basic stuff like position and size
         Frame.setLocationRelativeTo(null);
-        Frame.add(new MySnake());
-        Frame.setIconImage(new ImageIcon("src/com/sebi/dot.png").getImage());
         Frame.setSize(800,400);
-        Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Frame.add(new MySnake());                       //add the custom JPanel
+        Frame.setIconImage(new ImageIcon("src/com/sebi/dot.png").getImage());   //trying to add an ImageIcon, succeeded in my IDE, but as soon as I compiled the Jar, it stopped working
         Frame.setResizable(false);
-        Frame.setLocationRelativeTo(null);
-        Frame.setVisible(true);
-
+        Frame.setVisible(true);             //setVisible AFTER adding the JPanel
     }
 }
