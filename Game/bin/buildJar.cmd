850d5f8df31d15f72f734ce8a15b5929c238e42a
Caliduseb <46005624+Caliduseb@users.noreply.github.com>
2019-09-21 19:08:27 +0200

    compiled jar added delete statement in .cmd file to prevent java.lang.NoClassDefFoundError while using enums removed META-INF and manifest. hope it doesn't crash everything

--- Game/bin/buildJar.cmd
+++ Game/bin/buildJar.cmd
@@ -1,3 +1,5 @@
-echo off
+@echo off
+del Game.jar
+del %HOMEDRIVE%%HOMEPATH%\Desktop\Game.jar
 jar cmf .\TheGame.mf Game.jar .\com\sebi
 jar cmf .\TheGame.mf %HOMEDRIVE%%HOMEPATH%\Desktop\Game.jar .\com\sebi
